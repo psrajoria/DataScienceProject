009cd887479ab24e6806392cb3f9901ec15b18e9
psrajoria <psrajoria@outlook.com>
2021-06-26 00:32:23 +0530

    Product Model Setup

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    image = models.ImageField(upload_to='products', default='no_picture.jpg')
+    price = models.FloatField(help_text="in INR ruppes")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return f"{self.name} - {self.created.strftime('%d/%m/%Y')}"
