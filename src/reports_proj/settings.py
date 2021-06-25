b9a725da0053d1a7af9c73362162ae093a4c7cf6
psrajoria <psrajoria@outlook.com>
2021-06-26 00:16:03 +0530

    Finished with Setup

--- src/reports_proj/settings.py
+++ src/reports_proj/settings.py
@@ -37,7 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    ## Our Apps
+    'customers',
+    'products',
+    'profiles',
+    'reports',
+    'sales',
+
+    # 3rd Party
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -118,6 +131,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
