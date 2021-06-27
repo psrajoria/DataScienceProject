6924a5a199396574c9aa980e9c8d6d3b1ce389ab
psrajoria <psrajoria@outlook.com>
2021-06-27 21:09:07 +0530

    Horizontal filter in sales for positions

--- src/sales/admin.py
+++ src/sales/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-
+from django.db import models
 # Register your models here.
 
 from .models import Sale,Position,CSV
 
+### Horizontal filter to choose or remove positions
+class SaleAdmin(admin.ModelAdmin):
+    filter_horizontal = ("positions",)
+
 admin.site.register(Position)
-admin.site.register(Sale)
+admin.site.register(Sale,SaleAdmin)
 admin.site.register(CSV)
